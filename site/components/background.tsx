9d6c81566891ea34f2490e75b5020c85a086a3dc
machin3boy <amir.kashapov@protonmail.ch>
2024-10-18 21:15:02 +1100

    feat: shimmer button, blur in

--- site/components/background.tsx
+++ site/components/background.tsx
@@ -5,11 +5,11 @@ const Background: React.FC = () => (
   <div className="fixed inset-0 z-0">
     <Particles
       className="absolute inset-0 pointer-events-none"
-      quantity={125}
+      quantity={150}
       staticity={100}
       ease={70}
       size={0.03}
-      color="#fffbeb"
+      color="#fde68a"
     />
     <div className="pointer-events-none absolute inset-0 bg-[radial-gradient(ellipse_at_center,rgba(251,191,36,0.15),rgba(251,146,60,0.1)_70%,rgba(251,146,60,0)_100%)]" />
   </div>
--- site/components/hero.tsx
+++ site/components/hero.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { ChevronRight, Zap, Globe, Shield, Layers } from "lucide-react";
 import { BentoGrid, BentoCard } from "@/components/ui/bento-grid";
 import { cn } from "@/lib/utils";
+import ShimmerButton from "@/components/magicui/shimmer-button";
 import BlurIn from '@/components/magicui/blurin';  
 
 interface HeroProps {
@@ -15,21 +16,23 @@ const Hero: React.FC<HeroProps> = ({ onGetStarted }) => {
     <main className="flex flex-col items-center justify-center gap-6 z-10 relative">
       <BlurIn 
         word="AltVerse: Trustless, Instant Cross-Chain Swaps"
-        className="text-balance bg-gradient-to-br from-white from-30% to-white/60 bg-clip-text py-2 text-center text-3xl font-semibold leading-tight tracking-tighter text-transparent sm:text-4xl md:text-5xl"
+        className="text-balance bg-gradient-to-br from-white from-30% to-white/50 bg-clip-text py-2 text-center font-normal leading-tight tracking-tighter text-transparent sm:text-3xl md:text-4xl lg:text-[2.65rem]"
+        // className="bg-gradient-to-br from-white from-30% to-white/1 bg-clip-text py-2 mt-4 mb-4 sm:text-3xl md:text-4xl lg:text-5xl leading-none tracking-tighter text-transparent text-balance translate-y-[-0.6rem] pointer-events-none"
+        duration={2}
       />
-
       <div className="flex flex-col gap-4 sm:flex-row mb-4">
-        <button
-          onClick={onGetStarted}
-          className={cn(
-            "bg-white text-black shadow hover:bg-white/90",
-            "group relative inline-flex h-10 items-center justify-center gap-2 overflow-hidden whitespace-pre rounded-md px-4 py-2 text-sm font-semibold tracking-tighter",
-            "transform-gpu ring-offset-black transition-all duration-300 ease-out hover:ring-2 hover:ring-amber-500 hover:ring-offset-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-amber-500 focus-visible:ring-offset-2",
-          )}
-        >
-          Get Started
-          <ChevronRight className="size-4 translate-x-0 transition-all duration-300 ease-out group-hover:translate-x-1" />
-        </button>
+          <ShimmerButton
+            className="h-12 shadow-2xl"
+            shimmerColor="rgb(256 158 11)"
+            shimmerSize="0.12em"
+            shimmerDuration="2.5s"
+            background="black"
+            onClick={onGetStarted}
+          >
+            <span className="whitespace-pre-wrap px-8 text-center text-base font-bold leading-none tracking-tight text-amber-400 bg-black">
+              Get Started
+            </span>
+          </ShimmerButton>
       </div>
 
       <div className="w-full max-w-6xl mx-auto">
