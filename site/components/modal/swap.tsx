dd769ba1816dc2b31c716c0444eb0636d6154962
machin3boy <amir.kashapov@protonmail.ch>
2024-10-22 21:02:46 +1100

    feat: improved Swap, USDC tab content styling

--- site/components/modal/swap.tsx
+++ site/components/modal/swap.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { ChevronDown } from "lucide-react";
 import { BorderBeam } from "@/components/magicui/border-beam";
 import {
   Select,
@@ -27,36 +26,77 @@ const chains = [
 export default function Swap() {
   const [swapFromToken, setSwapFromToken] = useState("ALT ");
   const [swapToToken, setSwapToToken] = useState("wBTC");
+  const [amount, setAmount] = useState("");
+  const [receivedAmount, setReceivedAmount] = useState("");
+
+  const handleAmountChange = (e: any) => {
+    const value = e.target.value;
+
+    // Handle empty input case
+    if (value === "") {
+      setAmount("");
+      return;
+    }
+
+    // Only allow numbers and a single decimal point
+    const hasValidCharacters = /^[0-9.]*$/.test(value);
+    if (!hasValidCharacters) {
+      return;
+    }
+
+    // Count decimal points
+    const decimalCount = (value.match(/\./g) || []).length;
+    if (decimalCount > 1) {
+      return;
+    }
+
+    // If we get here, the input is valid - update the state
+    setAmount(value);
+  };
+
+  // Only show gray when the input is completely empty
+  const isGrayText = amount === "";
+
   return (
     <>
       <div className="relative rounded-lg">
         <BorderBeam
           borderWidth={2}
-          size={450}
+          size={350}
           duration={10}
           colorFrom="#F59E0B"
           colorTo="#1C1205"
         />
         <div className="bg-secondary p-4 rounded-lg space-y-6">
-          <div className="space-y-2">
+          <div className="space-y-4">
             <div className="flex justify-between items-center">
-              <span className="text-sm font-medium">You pay</span>
-              <Button variant="ghost" size="sm" className="text-xs">
-                Max
+              <span className="text-sm font-mono font-bold">You pay</span>
+              <Button
+                variant="ghost"
+                size="sm"
+                className="text-xs bg-amber-500/10 hover:bg-amber-500/20 text-amber-500"
+              >
+                max
               </Button>
             </div>
             <div className="flex items-center space-x-2">
               <Input
                 type="number"
+                value={amount}
+                onChange={handleAmountChange}
                 placeholder="0.0"
-                className="text-2xl font-mono bg-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                className={`text-2xl font-mono bg-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0 ${
+                  isGrayText
+                    ? "text-gray-500 placeholder:text-gray-500"
+                    : "text-white"
+                }`}
                 style={{
                   WebkitAppearance: "none",
                   MozAppearance: "textfield",
                 }}
               />
               <Select value={swapFromToken} onValueChange={setSwapFromToken}>
-                <SelectTrigger className="w-[120px]">
+                <SelectTrigger className="w-[120px] border-2 border-amber-500/10 font-semibold data-[state=open]:border-amber-500 focus:ring-0 focus:ring-offset-0 bg-amber-500/10 py-4">
                   <SelectValue>
                     <div className="flex items-center">
                       <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-2">
@@ -68,9 +108,13 @@ export default function Swap() {
                     </div>
                   </SelectValue>
                 </SelectTrigger>
-                <SelectContent>
+                <SelectContent className="bg-black text-white border-amber-500/20 border-2">
                   {tokens.map((token) => (
-                    <SelectItem key={token.symbol} value={token.symbol}>
+                    <SelectItem
+                      key={token.symbol}
+                      value={token.symbol}
+                      className="font-semibold data-[highlighted]:bg-amber-500 data-[highlighted]:text-white"
+                    >
                       <div className="flex items-center">
                         <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-2">
                           {token.icon}
@@ -85,24 +129,26 @@ export default function Swap() {
               </Select>
             </div>
           </div>
-          <div className="flex justify-center">
-            <ChevronDown className="w-6 h-6" />
-          </div>
-          <div className="space-y-2">
-            <span className="text-sm font-medium">You receive</span>
+          <div className="space-y-4 pt-4">
+            <span className="text-sm font-mono font-bold">You receive</span>
             <div className="flex items-center space-x-2">
               <Input
-                type="number"
+                type="text"
+                value={receivedAmount}
                 placeholder="0.0"
-                className="text-2xl font-mono bg-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0"
-                readOnly
+                className={`text-2xl font-mono bg-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0 ${
+                  receivedAmount === ""
+                    ? "text-gray-500 placeholder:text-gray-500"
+                    : "text-white"
+                }`}
                 style={{
                   WebkitAppearance: "none",
                   MozAppearance: "textfield",
                 }}
+                readOnly
               />
               <Select value={swapToToken} onValueChange={setSwapToToken}>
-                <SelectTrigger className="w-[120px]">
+                <SelectTrigger className="w-[120px] border-2 border-amber-500/10 font-semibold data-[state=open]:border-amber-500 focus:ring-0 focus:ring-offset-0 bg-amber-500/10 py-4">
                   <SelectValue>
                     <div className="flex items-center">
                       <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-2">
@@ -114,9 +160,13 @@ export default function Swap() {
                     </div>
                   </SelectValue>
                 </SelectTrigger>
-                <SelectContent>
+                <SelectContent className="bg-black text-white border-amber-500/20 border-2">
                   {tokens.map((token) => (
-                    <SelectItem key={token.symbol} value={token.symbol}>
+                    <SelectItem
+                      key={token.symbol}
+                      value={token.symbol}
+                      className="font-semibold data-[highlighted]:bg-amber-500 data-[highlighted]:text-white"
+                    >
                       <div className="flex items-center">
                         <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-2">
                           {token.icon}
@@ -134,18 +184,22 @@ export default function Swap() {
         </div>
       </div>
       <Select>
-        <SelectTrigger>
+        <SelectTrigger className="border-2 border-gray-700 font-semibold data-[state=open]:border-amber-500 focus:ring-0 focus:ring-offset-0 bg-transparent">
           <SelectValue placeholder="Select destination chain" />
         </SelectTrigger>
-        <SelectContent>
+        <SelectContent className="bg-black text-white border-amber-500/20 border-2">
           {chains.map((chain) => (
-            <SelectItem key={chain.id} value={chain.id}>
+            <SelectItem
+              key={chain.id}
+              value={chain.id}
+              className="font-semibold data-[highlighted]:bg-amber-500 data-[highlighted]:text-white"
+            >
               {chain.name}
             </SelectItem>
           ))}
         </SelectContent>
       </Select>
-      <Button className="w-full">Connect Wallet</Button>
+      <Button className="w-full">Swap</Button>
     </>
   );
 }
--- site/components/modal/usdc.tsx
+++ site/components/modal/usdc.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { ChevronDown } from "lucide-react";
 import { BorderBeam } from "@/components/magicui/border-beam";
 import NumberTicker from "@/components/magicui/number-ticker";
 import {
@@ -14,6 +13,32 @@ import {
 
 export default function USDC() {
   const [altcoinFromToken, setAltcoinFromToken] = useState("USDC");
+  const [amount, setAmount] = useState("");
+  const [receivedAmount, setReceivedAmount] = useState("");
+
+  const handleAmountChange = (e: any) => {
+    const value = e.target.value;
+
+    if (value === "") {
+      setAmount("");
+      return;
+    }
+
+    if (value === "0") {
+      setAmount("0");
+      return;
+    }
+
+    if (!isNaN(value) && parseFloat(value) >= 0) {
+      setAmount(
+        value.startsWith("0") && !value.startsWith("0.")
+          ? value.slice(1)
+          : value
+      );
+    }
+  };
+
+  const isGrayText = amount === "";
 
   return (
     <>
@@ -30,24 +55,34 @@ export default function USDC() {
       <div className="relative rounded-lg">
         <BorderBeam
           borderWidth={2}
-          size={450}
+          size={350}
           duration={10}
           colorFrom="#F59E0B"
           colorTo="#1C1205"
         />
         <div className="bg-secondary p-4 rounded-lg space-y-6">
-          <div className="space-y-2">
+          <div className="space-y-4">
             <div className="flex justify-between items-center">
-              <span className="text-sm font-medium">You pay</span>
-              <Button variant="ghost" size="sm" className="text-xs">
-                Max
+              <span className="text-sm font-mono font-bold">You pay</span>
+              <Button
+                variant="ghost"
+                size="sm"
+                className="text-xs bg-amber-500/10 hover:bg-amber-500/20 text-amber-500"
+              >
+                max
               </Button>
             </div>
             <div className="flex items-center space-x-2">
               <Input
                 type="number"
+                value={amount}
+                onChange={handleAmountChange}
                 placeholder="0.0"
-                className="text-2xl font-mono bg-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                className={`text-2xl font-mono bg-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0 ${
+                  isGrayText
+                    ? "text-gray-500 placeholder:text-gray-500"
+                    : "text-white"
+                }`}
                 style={{
                   WebkitAppearance: "none",
                   MozAppearance: "textfield",
@@ -57,7 +92,7 @@ export default function USDC() {
                 value={altcoinFromToken}
                 onValueChange={setAltcoinFromToken}
               >
-                <SelectTrigger className="w-[120px]">
+                <SelectTrigger className="w-[120px] border-2 border-amber-500/10 font-semibold data-[state=open]:border-amber-500 focus:ring-0 focus:ring-offset-0 bg-amber-500/10">
                   <SelectValue>
                     <div className="flex items-center">
                       <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-2">
@@ -69,8 +104,11 @@ export default function USDC() {
                     </div>
                   </SelectValue>
                 </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="USDC">
+                <SelectContent className="bg-black text-white border-amber-500/20 border-2">
+                  <SelectItem
+                    value="USDC"
+                    className="font-semibold data-[highlighted]:bg-amber-500 data-[highlighted]:text-white"
+                  >
                     <div className="flex items-center">
                       <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-2">
                         $
@@ -78,7 +116,10 @@ export default function USDC() {
                       <span className="font-mono w-12">USDC</span>
                     </div>
                   </SelectItem>
-                  <SelectItem value="ALT">
+                  <SelectItem
+                    value="ALT"
+                    className="font-semibold data-[highlighted]:bg-amber-500 data-[highlighted]:text-white"
+                  >
                     <div className="flex items-center">
                       <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-2">
                         A
@@ -90,29 +131,33 @@ export default function USDC() {
               </Select>
             </div>
           </div>
-          <div className="flex justify-center">
-            <ChevronDown className="w-6 h-6" />
-          </div>
-          <div className="space-y-2">
-            <span className="text-sm font-medium">You receive</span>
+          <div className="space-y-4 pt-4">
+            <span className="text-sm font-mono font-bold">You receive</span>
             <div className="flex items-center space-x-2">
               <Input
                 type="number"
+                value={receivedAmount}
                 placeholder="0.0"
-                className="text-2xl font-mono bg-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0"
-                readOnly
+                className={`text-2xl font-mono bg-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0 ${
+                  receivedAmount === ""
+                    ? "text-gray-500 placeholder:text-gray-500"
+                    : "text-white"
+                }`}
                 style={{
                   WebkitAppearance: "none",
                   MozAppearance: "textfield",
                 }}
+                readOnly
               />
-              <div className="w-[120px] h-10 px-3 py-2 rounded-md border border-input bg-background text-sm flex items-center">
-                <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-2">
-                  {altcoinFromToken === "USDC" ? "A" : "$"}
-                </span>
-                <span className="font-mono w-12">
-                  {(altcoinFromToken === "USDC" ? "ALT " : "USDC").padEnd(4)}
-                </span>
+              <div className="h-9 w-[120px] border-2 border-amber-500/10 font-semibold bg-amber-500/10 rounded-md flex items-center px-3">
+                <div className="flex items-center">
+                  <span className="w-6 h-6 rounded-full bg-primary text-primary-foreground flex items-center justify-center mr-3">
+                    {altcoinFromToken === "USDC" ? "A" : "$"}
+                  </span>
+                  <span className="font-mono w-16">
+                    {(altcoinFromToken === "USDC" ? "ALT " : "USDC").padEnd(4)}
+                  </span>
+                </div>
               </div>
             </div>
           </div>
